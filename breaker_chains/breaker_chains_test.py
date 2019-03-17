66c44c69ebe142f6de5aa8a56ed8278b3dd0b0cf
codecakes <akurnya@gmail.com>
2019-03-17 14:20:19 +0530

    working state

--- breaker_chains/breaker_chains_test.py
+++ breaker_chains/breaker_chains_test.py
@@ -1,10 +1,11 @@
 import unittest
-from random import choice
+from unittest import TestCase
+from random import choice, shuffle
 
-from .breaker_chains import _KINGDOMS, extract_keyword
+from .breaker_chains import _KINGDOMS, ALLEGIANCE, extract_keyword, create_msg_packet, Ruler
 
 
-class TestRunToss(unittest.TestCase):
+class TestRunToss(TestCase):
 
     def setUp(self):
         """
@@ -18,6 +19,7 @@ class TestRunToss(unittest.TestCase):
         self.kingdoms = _KINGDOMS
         self.kingdoms_owners = list(self.kingdoms.keys())
         self.pick_one = lambda: choice(self.kingdoms_owners)
+        self.allegiance: ALLEGIANCE = {}
 
     def testDetectEmblem(self):
         pandaMsg = 'a1d22n333a4444p'
@@ -25,6 +27,37 @@ class TestRunToss(unittest.TestCase):
         expected = 'panda'
         self.assertEqual(expected.lower(), extract_keyword(pandaMsg, receiver),
                          'keyword should matches receiving kingdoms emblem')
+
+    def testPreRun(self):
+        ruler = Ruler(contenders=['Ice', 'Space', 'Air'])
+        ruler._pre()
+        self.assertTrue(ruler._subject_kingdoms, 'subject kingdoms should populate.')
+        self.assertTrue(ruler._messages, 'subject kingdoms should populate.')
+        self.assertTrue(ruler._msg_kingdom, 'msgs should populate.')
+        self.assertTrue(ruler._contenders, 'there should be contenders.')
+
+    def testAddContenders(self):
+        ruler = Ruler()
+        ruler.add_contenders(['Ice', 'Space', 'Air'])
+        self.assertTrue(ruler._contenders)
+
+    def testPostRun(self):
+        ruler = Ruler(contenders=['Ice', 'Space', 'Air'])
+        ruler._run()
+        ruler.get_winner()
+        self.assertTrue(ruler._nominations, 'there should be nominations.')
+        self.assertTrue(ruler.allegiance_map, 'should have winners.')
+
+    def testShufflePop(self):
+        ruler = Ruler(contenders=['Ice', 'Space', 'Air'])
+        ruler._pre()
+        self.assertTrue(ruler._shuffle_choice(ruler._msg_kingdom), 'should return randomly.')
+
+    def testBeingNomination(self):
+        ruler = Ruler(contenders=['Ice', 'Space', 'Air'])
+        ruler._pre()
+        ruler._begin_nomination()
+        self.assertTrue(ruler.ballot, 'ballot should populate.')
 
 
 if __name__ == '__main__':
